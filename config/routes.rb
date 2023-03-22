cef4b2ac1bd172c10b382836d2dd5be1a51b3d98
Glen <glen.kimani@student.moringaschool.com>
2023-03-22 13:17:49 +0300

    Commit

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  resources :restaurants, only: [:index,:show,:destroy]
-  resources :restaurant_pizzas, only:[:create]
-  resources :pizzas, only: [:index]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
--- test/controllers/restaurant_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class RestaurantControllerTest < ActionDispatch::IntegrationTest
-  test "should get pizzas" do
-    get restaurant_pizzas_url
-    assert_response :success
-  end
-end
